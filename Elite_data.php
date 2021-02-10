598267926081f303b7ede8cab6ead328b3511ec8
Abdulazizregas <bocahceplak@gmail.com>
2021-02-11 02:45:44 +0700

    Add files via upload

---
+++ Elite_data.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/* CONFIG BY FChyd
+SUBSCRIBE CHANNEL AUTHOR E. SCRIPTER
+COPYRIGHT 2020
+ILEGAL TO MODIFY / DECRYPT THIS SCRIPT
+THANKS
+*/
+
+
+# DATA LOGIN
+$email = "69vowz@gmail.com";
+
+$password = "Newmash1";
+
+
+
+# SEARCH IN GOOGLE "MY USER AGENT"
+$useragent = "Mozilla/5.0 (Linux; Android 9; ASUS_X01BDA) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
